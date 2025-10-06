e6c22f23de7ebfe6b16d01ff1651fb38ceae9cb6
MaksPyvo <pyvovar.maksym.vol@gmail.com>
2025-10-05 20:41:45 -0400

    improved factorial stuff

--- factorial.py
+++ factorial.py
@@ -3,18 +3,19 @@
 import time
 
 final_list = []
-
+# Function to calculate factorial
 def factorial(n):
 
     time.sleep(.1)
 
-    factorial = 1
+    result = 1
 
     for i in range(1, n+1):
-        factorial = factorial * i
+        result = result * i
 
-    return factorial
+    return result
 
+# Function to calculate sum of factorials
 def sum_factorial():
 
     for i in range(50):
